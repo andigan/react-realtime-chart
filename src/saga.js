62896b35545d8d7f52abd35af9ee289c022d1fce
andigan <andrew.nease.code@gmail.com>
2018-01-04 17:17:16 -0500

    clean up redux-saga

--- src/saga.js
+++ src/saga.js
@@ -1,25 +1,21 @@
-
 import {delay} from 'redux-saga';
 
 import {take, put, call, race, all } from 'redux-saga/effects';
 
 import axios from 'axios';
 
-const BITCOIN_ENDPOINT = 'https://api.coindesk.com/v1/bpi/currentprice.json';
+import Promise from 'bluebird';
+
+var pollFailCount = 0,
+    pollFailCountLimit = 10;
+
+const ENDPOINT_BITCOIN = 'https://api.coindesk.com/v1/bpi/currentprice.json';
 
 const API_DELAY = 2000;
 
-const POLL_START = 'POLL_START';
-const POLL_STOP = 'POLL_STOP';
-const GET_DATA_SUCCESS = 'GET_DATA_SUCCESS';
-const GET_DATA_FAILURE = 'GET_DATA_FAILURE';
-
-const getDataSuccessAction = payload => ({ type: GET_DATA_SUCCESS, payload });
-const getDataFailureAction = payload => ({ type: GET_DATA_FAILURE, payload });
-
 export function* initialSaga() {
   yield all([
-    watchPollSaga(),
+    watchForPollActions(),
     initialize()
 ]);
 }
@@ -28,32 +24,40 @@ function* initialize() {
   yield put({type: "POLL_START"});
 }
 
-function* watchPollSaga() {
+function* watchForPollActions() {
 	while (true) {
-    console.log('1: watch start');
-  	yield take(POLL_START);
+    console.log('1: watch for poll actions');
+  	yield take("POLL_START");
     console.log('2: POLL_START taken');
     yield race([
-      call(pollSaga),
-      take(POLL_STOP)
+      call(startPollLoop),
+      take("POLL_STOP")
     ]);
   }
 }
 
-function* pollSaga(action) {
-  console.log("3: pollSaga started");
+function* startPollLoop(action) {
+  console.log("3: startPollLoop started");
   while (true) {
     try {
-      const { data } = yield call(() => axios({ url: BITCOIN_ENDPOINT }));
 
-      let constructedPayload = {
-        chartData: parseFloat(data.bpi.USD.rate.replace(/,/g, '')) + Math.floor(Math.random() * 10),
+      const responsesPromiseObject = yield Promise.props({
+        bitcoinResponse: yield call(() => axios({ url: ENDPOINT_BITCOIN }))
+      }).then((result) => result);
+
+      let payload = {
+        chartData: parseFloat(responsesPromiseObject.bitcoinResponse.data.bpi.USD.rate.replace(/,/g, '')) + Math.floor(Math.random() * 10)
       };
 
-      yield put(getDataSuccessAction(constructedPayload));
+      yield put ({type: "GET_DATA_SUCCESS", payload});
       yield call(delay, API_DELAY);
     } catch (err) {
-      yield put(getDataFailureAction(err));
+        yield put ({type: "GET_DATA_FAILURE", err});
+        pollFailCount += 1;
+        if (pollFailCount >= pollFailCountLimit) {
+          console.log(`Poll Fail Count: ${pollFailCount}.  Polling stopped.  ${err.config ? err.config.url : null}`);
+          yield put ({type: "POLL_STOP"});
+        }
     }
   }
 }
